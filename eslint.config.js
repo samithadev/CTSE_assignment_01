6c046020e9420ce639e710a5b4a5eec4699e0a9c
Samitha Wickramasinghe <contact.samitha@gmail.com>
2025-04-15 13:26:57 +0530

    Migrate to ESLint v9 config format

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    extends: ['eslint:recommended'],
+    rules: {
+      // Add your custom linting rules here
+    },
+  };
