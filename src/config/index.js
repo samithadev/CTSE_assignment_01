23aa4e66f53f4c867339b620937de54c100b273c
Samitha Wickramasinghe <contact.samitha@gmail.com>
2025-04-15 15:53:16 +0530

    update mongodb name

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/product-catalog',
+  MONGODB_DB_NAME: process.env.MONGODB_NAME || 'product_catalog',
   // Add other configuration variables as needed
 };
